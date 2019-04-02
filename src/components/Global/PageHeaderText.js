307c66c71e27e2f0f55412e953b78e096f2cfeeb
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-04-02 11:26:04 +0200

    added global styling

--- src/components/Global/PageHeaderText.js
+++ src/components/Global/PageHeaderText.js
@@ -1,7 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledPageHeaderText = styled.div``;
+const StyledPageHeaderText = styled.div`
+  left: 82px;
+  h2 {
+    width: 634px;
+    height: 94px;
+    font-weight: 300;
+    font-size: 64px;
+  }
+  p {
+    font-weight: 300;
+    font-size: 24px;
+    width: 634px;
+    height: 106px;
+  }
+  a {
+    width: 634px;
+    height: 36px;
+    font-weight: 500;
+    font-size: 24px;
+    color: #1f1f1f;
+    text-decoration: none;
+  }
+`;
 
 const PageHeaderText = props => {
   return (
@@ -12,10 +34,9 @@ const PageHeaderText = props => {
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        velit esse cillum dolore eu fugiat nulla pariatur.
       </p>
+      <a href="">Länk</a>
     </StyledPageHeaderText>
   );
 };
