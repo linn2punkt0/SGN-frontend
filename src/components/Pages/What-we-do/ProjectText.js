ce7f64573086892b2276e2ce1f79181fd14cd0a4
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-04-02 11:26:21 +0200

    styled ProjectText-component

--- src/components/Pages/What-we-do/ProjectText.js
+++ src/components/Pages/What-we-do/ProjectText.js
@@ -1,7 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledProjectText = styled.div``;
+const StyledProjectText = styled.div`
+  left: 82px;
+  h4 {
+    width: 467px;
+    height: 53px;
+    font-weight: 300;
+    font-size: 36px;
+  }
+  p {
+    width: 307px;
+    height: 397px;
+    font-weight: 300;
+    font-size: 24px;
+  }
+  a {
+    width: 634px;
+    height: 36px;
+    font-weight: 500;
+    font-size: 24px;
+    color: #1f1f1f;
+    text-decoration: none;
+  }
+`;
 
 const ProjectText = props => {
   return (
@@ -11,10 +33,7 @@ const ProjectText = props => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        commodo consequat.
       </p>
       <a href="">Link to project</a>
     </StyledProjectText>
