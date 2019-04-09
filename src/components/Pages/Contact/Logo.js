bcea17daca72b94bbb6f70018d37e526e1e6874c
Laureta Dzika <xhika_l@hotmail.com>
2019-04-09 22:23:27 +0200

    testing mobile view in contact-page

--- src/components/Pages/Contact/Logo.js
+++ src/components/Pages/Contact/Logo.js
@@ -5,20 +5,37 @@ import PropTypes from 'prop-types';
 
 const StlyedLogo = styled.div`
 	display: grid;
-	align-self: center;
-	justify-self: center;
+
+	img {
+		justify-self: center;
+		height: 40%;
+	}
+	h1, p, h3 {
+		margin-left: 5rem
+	}
+	h3 {
+		font-weight: 400;
+	}
 `;
 
 const Logo = props => {
     return (
         <StlyedLogo>
-        	<img src={props.image} />
+	        	<img src={props.image} />
+	        	<h1>{props.header}</h1>
+	        	<p>{props.text}</p>
+	        	<h3>{props.email}</h3>
+	        	<h3>{props.phone}</h3>
         </StlyedLogo>
     );
 };
 
 Logo.propTypes = {
+	header: PropTypes.string,
     image: PropTypes.string, 
+    text: PropTypes.string, 
+    email: PropTypes.string, 
+    phone: PropTypes.string, 
 }
 
 export default Logo;
