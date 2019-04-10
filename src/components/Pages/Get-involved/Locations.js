c4f599eea8aee86262f0808b0dcce6f80061534f
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-04-10 14:45:48 +0200

    Locations scroll componentd added

--- src/components/Pages/Get-involved/Locations.js
+++ src/components/Pages/Get-involved/Locations.js
@@ -1,12 +1,42 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledLocations = styled.div``;
+const StyledLocations = styled.div`
+  width: 644px;
+  height: 501px;
+  left: 78px;
+  overflow-y: scroll;
+
+  li {
+    width: 635px;
+    height: 106px;
+    left: 82px;
+    font-family: Heebo;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: normal;
+    list-style: none;
+  }
+`;
 
 const Locations = props => {
   return (
     <StyledLocations>
       <ul>
+        <li>location</li>
+        <li>location</li>
+        <li>location</li>
+        <li>location</li>
+        <li>location</li>
+        <li>location</li>
+        <li>location</li>
+        <li>location</li>
+        <li>location</li>
+        <li>location</li>
+        <li>location</li>
+        <li>location</li>
+        <li>location</li>
         <li>location</li>
       </ul>
     </StyledLocations>
