42685812a9bb714d2abd32579464f5c6b515b951
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-04-14 18:02:02 +0200

    work in progress - get involved

--- src/components/Pages/Get-involved/Find.js
+++ src/components/Pages/Get-involved/Find.js
@@ -1,14 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import Locations from "./Locations";
+import Activities from "./Activities";
 
 const StyledFind = styled.div`
   margin-left: 82px;
   position: relative;
   h4 {
     font-weight: 300;
-    font-size: 36px;
-    margin: 40px 0 2px 0;
+    font-size: 4vh;
+    margin: 4vh 0 2px 0;
   }
   .grey {
     margin: 0;
@@ -35,15 +36,19 @@ const StyledFind = styled.div`
 `;
 
 const Find = props => {
+  {
+    console.log(props.content);
+  }
   return (
     <StyledFind>
-      <h4>Find us here/Activities</h4>
-      <h4 className="grey">These are our branches/Activities</h4>
-      <Locations />
+      <h4>{props.content.title}</h4>
+      <h4 className="grey">{props.content.title_2}</h4>
+      {props.content.title === "Our locations" && <Locations />}
+      {props.content.title === "Activities" && <Activities />}
       <div className="startYourOwn">
-        <h5>No branch/Got an idea?</h5>
+        <h5>{props.content.question}</h5>
         <a>
-          <h5>Start one/ Start it yourself!</h5>
+          <h5>{props.content.start_one}</h5>
         </a>
       </div>
     </StyledFind>
--- src/components/Pages/Get-involved/GetInvolved.js
+++ src/components/Pages/Get-involved/GetInvolved.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Find from "./Find";
 import PageHeaderText from "../../Global/PageHeaderText";
@@ -11,12 +11,29 @@ const StyledGetInvolved = styled.div`
 `;
 
 const GetInvolved = props => {
+  const [content, setContent] = useState(null);
+
+  useEffect(() => {
+    fetchContent();
+  }, []);
+
+  const fetchContent = () => {
+    fetch(`http://wordplate.test/wp-json/wp/v2/pages?slug=get-involved`)
+      .then(response => response.json())
+      .then(data => {
+        setContent(data[0].acf);
+      })
+      .catch(error => console.error(error));
+  };
+
   return (
     <StyledGetInvolved>
-      <PageHeaderText />
+      {content && <PageHeaderText content={content.header} />}
       <ImageContainer background="lightpink" />
-      <Find />
+      {content && <Find content={content.locations} />}
       <ImageContainer background="blue" />
+      {content && <Find content={content.activities} />}
+      <ImageContainer background="green" />
     </StyledGetInvolved>
   );
 };
