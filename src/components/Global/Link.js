7a90d006c896b8e93003a47d2ea88ae70373bb9f
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-04-17 10:36:43 +0200

    removed News in menu

--- src/components/Global/Link.js
+++ src/components/Global/Link.js
@@ -17,7 +17,9 @@ const LinksForNav = styled.div`
     transform: translate(50%);
   
     /*VAD SKA JAG SÄGA HÄR FÖR ATT DEN SKA TRANSFPORMA*/
-  /* ${props => props.transformlinks && `
+  /* ${props =>
+    props.transformlinks &&
+    `
     transform: translate(0%);
     `} */
     > * {
@@ -26,35 +28,98 @@ const LinksForNav = styled.div`
     transition:1s transform;
     /* transform: translate(50%); */
     
-    /* ${props => props.transforLinks && `
+    /* ${props =>
+      props.transforLinks &&
+      `
     transform: translate(0%);
     `} */
     }
-    `
+    `;
 
-const NavLinks = (props) =>{
-    console.log(props)
-// render(props){
-    // console.log(props)
+const NavLinks = props => {
+  console.log(props);
+  // render(props){
+  // console.log(props)
 
-    let delayIncrement = 1;
+  let delayIncrement = 1;
 
-    return (
-
-    <LinksForNav >
-        <Link onClick={props.closeMenu} style={{transform: props.transforlinks ? 'translate(0)' : 'translate(50%)', transitionDelay:`0.${delayIncrement}s`}} to="/">Home</Link>
-        {/* <BarOne style={!props.closeMenu ? {transform: "rotate(-45deg) translate(-1vh, 1vw"} : null}/> */}
-        <Link onClick={props.closeMenu} style={{transform: props.transforlinks ? 'translate(0)' : 'translate(50%)', transitionDelay:`0.${(delayIncrement++)}s`}} to="/get-involved">Get involved</Link>
-        <Link onClick={props.closeMenu} style={{transform: props.transforlinks ? 'translate(0)' : 'translate(50%)', transitionDelay:`0.${(delayIncrement++)}s`}} to="/who-we-are">Who we are</Link>
-        <Link onClick={props.closeMenu} style={{transform: props.transforlinks ? 'translate(0)' : 'translate(50%)', transitionDelay:`0.${(delayIncrement++)}s`}} to="/what-we-do">What we do</Link>
-        <Link onClick={props.closeMenu} style={{transform: props.transforlinks ? 'translate(0)' : 'translate(50%)', transitionDelay:`0.${(delayIncrement++)}s`}} to="/get-involved">Membership</Link>
-        {/* Länka till botten av Get involved */}
-        <Link onClick={props.closeMenu} style={{transform: props.transforlinks ? 'translate(0)' : 'translate(50%)', transitionDelay:`0.${(delayIncrement++)}s`}} to="/">Donations</Link>
-        {/* Länka till botten av Home */}
-        <Link onClick={props.closeMenu} style={{transform: props.transforlinks ? 'translate(0)' : 'translate(50%)', transitionDelay:`0.${(delayIncrement++)}s`}} to="/news">News</Link>
-        <Link onClick={props.closeMenu} style={{transform: props.transforlinks ? 'translate(0)' : 'translate(50%)', transitionDelay:`0.${(delayIncrement++)}s`}} to="/contact">Contact</Link>
+  return (
+    <LinksForNav>
+      <Link
+        onClick={props.closeMenu}
+        style={{
+          transform: props.transforlinks ? "translate(0)" : "translate(50%)",
+          transitionDelay: `0.${delayIncrement}s`
+        }}
+        to="/"
+      >
+        Home
+      </Link>
+      {/* <BarOne style={!props.closeMenu ? {transform: "rotate(-45deg) translate(-1vh, 1vw"} : null}/> */}
+      <Link
+        onClick={props.closeMenu}
+        style={{
+          transform: props.transforlinks ? "translate(0)" : "translate(50%)",
+          transitionDelay: `0.${delayIncrement++}s`
+        }}
+        to="/get-involved"
+      >
+        Get involved
+      </Link>
+      <Link
+        onClick={props.closeMenu}
+        style={{
+          transform: props.transforlinks ? "translate(0)" : "translate(50%)",
+          transitionDelay: `0.${delayIncrement++}s`
+        }}
+        to="/who-we-are"
+      >
+        Who we are
+      </Link>
+      <Link
+        onClick={props.closeMenu}
+        style={{
+          transform: props.transforlinks ? "translate(0)" : "translate(50%)",
+          transitionDelay: `0.${delayIncrement++}s`
+        }}
+        to="/what-we-do"
+      >
+        What we do
+      </Link>
+      <Link
+        onClick={props.closeMenu}
+        style={{
+          transform: props.transforlinks ? "translate(0)" : "translate(50%)",
+          transitionDelay: `0.${delayIncrement++}s`
+        }}
+        to="/get-involved"
+      >
+        Membership
+      </Link>
+      {/* Länka till botten av Get involved */}
+      <Link
+        onClick={props.closeMenu}
+        style={{
+          transform: props.transforlinks ? "translate(0)" : "translate(50%)",
+          transitionDelay: `0.${delayIncrement++}s`
+        }}
+        to="/"
+      >
+        Donations
+      </Link>
+      {/* Länka till botten av Home */}
+      <Link
+        onClick={props.closeMenu}
+        style={{
+          transform: props.transforlinks ? "translate(0)" : "translate(50%)",
+          transitionDelay: `0.${delayIncrement++}s`
+        }}
+        to="/contact"
+      >
+        Contact
+      </Link>
     </LinksForNav>
-    )
-}
+  );
+};
 
-export default NavLinks
+export default NavLinks;
