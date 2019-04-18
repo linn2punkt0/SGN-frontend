a0cb49e1228dbd247d1bf2d9e874f227fa253431
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-04-18 10:29:02 +0200

    form-styling

--- src/components/Pages/Get-involved/MemberForm.js
+++ src/components/Pages/Get-involved/MemberForm.js
@@ -10,7 +10,7 @@ const StyledMemberForm = styled.div`
   align-items: center;
 
   .container {
-    max-width: 500px;
+    max-width: 600px;
     flex-direction: column;
     justify-content: center;
     align-items: center;
@@ -24,9 +24,13 @@ const StyledMemberForm = styled.div`
     .grey {
       margin: 0;
       color: rgba(31, 31, 31, 0.45);
+      margin-bottom: 8vh;
     }
     p {
       text-align: center;
+      font-weight: 300;
+      font-size: 3vh;
+      margin-bottom: 10vh;
     }
     form {
       display: flex;
@@ -52,11 +56,26 @@ const StyledMemberForm = styled.div`
     }
     label {
       color: rgba(113, 113, 113, 0.7);
+      margin-bottom: 20px;
     }
     input {
       width: 100%;
       border: none;
       border-bottom: 1px solid black;
+      margin-bottom: 28px;
+    }
+    .checkbox input[type="checkbox"] {
+      opacity: 0;
+    }
+
+    .checkbox label::before {
+      content: "";
+      display: inline-block;
+
+      height: 16px;
+      width: 16px;
+
+      border: 1px solid;
     }
   }
 `;
@@ -106,9 +125,9 @@ const MemberForm = props => {
             </div>
           </div>
           <label htmlFor="refugee">Refugee</label>
-          <input name="refugee" type="checkbox" />
+          <input name="refugee" type="checkbox" className="checkbox" />
           <label htmlFor="volunter">Volunter</label>
-          <input name="volunter" type="checkbox" />
+          <input name="volunter" type="checkbox" className="checkbox" />
           <label htmlFor="offer">
             This is what i can offer (knowledge, materials, locales etc.)
           </label>
@@ -119,7 +138,7 @@ const MemberForm = props => {
             Yes, I would like for you to match my competence with suitable
             available jobs
           </label>
-          <input name="match" type="checkbox" />
+          <input name="match" type="checkbox" className="checkbox" />
         </form>
       </div>
     </StyledMemberForm>
