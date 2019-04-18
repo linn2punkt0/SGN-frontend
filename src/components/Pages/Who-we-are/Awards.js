3e5c9e263e9bb86a9ed9ea43a6fff31c11991383
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-04-18 15:26:48 +0200

    continued work on WhoWeAre

---
+++ src/components/Pages/Who-we-are/Awards.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledAwards = styled.div`
+  width: 50%;
+  height: 100vh;
+  ul {
+    list-style: none;
+  }
+`;
+
+const Awards = props => {
+  return <StyledAwards>Awards here</StyledAwards>;
+};
+
+export default Awards;
