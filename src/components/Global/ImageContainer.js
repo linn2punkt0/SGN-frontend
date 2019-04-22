fbb6587661b4b23a19e265c56d95efe01a3287c7
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-04-22 22:34:45 +0200

    added images to Home-page

--- src/components/Global/ImageContainer.js
+++ src/components/Global/ImageContainer.js
@@ -2,7 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledImageContainer = styled.div`
-  background-color: ${props => props.background};
+  background: url(${props => props.background});
+  background-position: center;
+  background-size: cover;
   width: 50%;
   @media (min-width: 320px) and (max-width: 767px) {
     height: 50vh;
--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -7,34 +7,36 @@ import ContactContainer from "./Contact.js";
 // import Footer from "../../Global/Footer.js";
 import Container from "../../Global/Container";
 import Logo from "./Logo.svg";
+import ImageOne from "../../../images/get-involved_landing-page_illustration.png";
+import ImageTwo from "../../../images/who-we-arelanding-page_illustration.png";
+import ImageThree from "../../../images/what-we-do_landing-page_illustration.png";
 
 const StyledLogo = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-height: 100vh;
-width: 50%;
-div {
-    background: #EBDCCD;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 50%;
+  div {
+    background: #ebdccd;
     border-radius: 50%;
-    height:300px;
+    height: 300px;
     width: 300px;
-}
-img {
-  height:350px;
-}
-img:hover {
-  transform: scale3d(1.1, 1.1, 1.1);
-  transition: all 1s ease-in-out;
-}
-div:hover {
-  transform: translate3d(10px, 0px, 0px) 
-             skew(-5deg, 4deg); 
-  transform-style: preserve-3d; 
-  will-change: transform;
-  transition: all 1s ease-in-out;
-}
-`
+  }
+  img {
+    height: 350px;
+  }
+  img:hover {
+    transform: scale3d(1.1, 1.1, 1.1);
+    transition: all 1s ease-in-out;
+  }
+  div:hover {
+    transform: translate3d(10px, 0px, 0px) skew(-5deg, 4deg);
+    transform-style: preserve-3d;
+    will-change: transform;
+    transition: all 1s ease-in-out;
+  }
+`;
 
 const StyledHome = styled.div`
   position: relative;
@@ -46,34 +48,30 @@ const StyledHome = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   overflow-x: hidden;
-    a {
+  a {
     /* height: 36px; */
     font-weight: 500;
     font-size: 24px;
-    color: #FFFFFF;
+    color: #ffffff;
     text-decoration: none;
   }
 
-  @media (min-width: 320px) and (max-width: 767px){
-  position: relative;
-  flex-direction: column;
-  width: 100vw;
-  height: 100%;
-  background-attachment: fixed;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  overflow-x: hidden;
- 
+  @media (min-width: 320px) and (max-width: 767px) {
+    position: relative;
+    flex-direction: column;
+    width: 100vw;
+    height: 100%;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    overflow-x: hidden;
   }
 `;
-
-
 
 const Home = props => {
   // Fetch home-page from api and set custom-field-content to content-hook.
   const [content, setContent] = useState(null);
-
 
   useEffect(() => {
     fetchContent();
@@ -92,25 +90,33 @@ const Home = props => {
     <StyledHome>
       <Container flexDirection="column">
         <PageHeaderText content={content ? content.header : ""} />
-          <StyledLogo>
-            <div>
-              <img src={Logo} alt="Logo"/>
-            </div>
-          </StyledLogo>
+        <StyledLogo>
+          <div>
+            <img src={Logo} alt="Logo" />
+          </div>
+        </StyledLogo>
       </Container>
 
       <Container>
-        <TextContainer content={content ? content.get_involved : ""}backgroundButton="#0021B8"/>
-        <ImageContainer background="#2703BB" />
-      </Container>
-      <Container>
-        <TextContainer content={content ? content.who_we_are : ""}  backgroundButton="#F9DC0E" 
+        <TextContainer
+          content={content ? content.get_involved : ""}
+          backgroundButton="#0021B8"
         />
-        <ImageContainer background="#F9DC0E" />
+        <ImageContainer background={ImageOne} />
       </Container>
       <Container>
-        <TextContainer content={content ? content.what_we_do : ""} backgroundButton="#F43F2A" />
-        <ImageContainer background="#F43F2A" />
+        <TextContainer
+          content={content ? content.who_we_are : ""}
+          backgroundButton="#F9DC0E"
+        />
+        <ImageContainer background={ImageTwo} />
+      </Container>
+      <Container>
+        <TextContainer
+          content={content ? content.what_we_do : ""}
+          backgroundButton="#F43F2A"
+        />
+        <ImageContainer background={ImageThree} />
       </Container>
       <ContactContainer
         title="Wanna help out?"
@@ -123,6 +129,5 @@ const Home = props => {
     </StyledHome>
   );
 };
-
 
 export default Home;
