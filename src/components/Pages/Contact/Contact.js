d2a08c346a03d009873d0d2f50a2f69facae7bf9
Laureta Dzika <xhika_l@hotmail.com>
2019-04-23 15:04:15 +0200

    Update Contact.js

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -74,7 +74,7 @@ const StyledContact = styled.div`
         }
     }
     .personContainerSmall {
-        margin-top: 20vh;
+        margin-top: 5vh;
         margin-left: 2vw;
         display: grid;
         grid-template-columns: 1fr 1fr 1fr;
