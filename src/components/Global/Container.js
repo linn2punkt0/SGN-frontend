e159c88ed25061d0c41454a99a1f9c8575f99a65
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-04-24 13:05:00 +0200

    container 200vh in mobile

--- src/components/Global/Container.js
+++ src/components/Global/Container.js
@@ -7,18 +7,19 @@ const ContainerForContent = styled.div`
   height: 100vh;
   width: 100vw;
   @media (min-width: 320px) and (max-width: 767px) {
-    flex-direction: ${props => props.flexDirection || 'column-reverse'}
+    flex-direction: ${props => props.flexDirection || "column-reverse"};
+    height: 200vh;
   }
 `;
 
 const ImageContainer = styled.div`
-width: 50vw;
-height: 100vh;
-background-color: red;  
- @media (min-width: 320px) and (max-width: 767px) {
+  width: 50vw;
+  height: 100vh;
+  background-color: red;
+  @media (min-width: 320px) and (max-width: 767px) {
     width: 100vw;
     height: 50vh;
- }
+  }
 `;
 
 const TextContainer = styled.div`
@@ -31,9 +32,12 @@ const TextContainer = styled.div`
   }
 `;
 
-
 const Container = props => {
-  return <ContainerForContent {...props} style={props.reverseRow}>{props.children}</ContainerForContent>;
+  return (
+    <ContainerForContent {...props} style={props.reverseRow}>
+      {props.children}
+    </ContainerForContent>
+  );
 };
 
 export default Container;
